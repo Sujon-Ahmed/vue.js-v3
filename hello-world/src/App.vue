a9b075f2369adc730fab36e9b6342e94a7c0c272
Sujon-Ahmed <sujonahmed5284@gmail.com>
2022-12-14 22:54:31 +0600

    new project setup

---
+++ hello-world/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>{{ greet }} {{ name }}</h2>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      greet: 'Hello',
+      name: 'Sujon Ahmed',
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 50px;
+}
+</style>
