1b86c490bf2868adb378204c1978f29522dd503d
Sujon-Ahmed <sujonahmed5284@gmail.com>
2022-12-15 22:41:12 +0600

    binding classes

--- vue-basic/src/App.vue
+++ vue-basic/src/App.vue
@@ -4,6 +4,22 @@
   <h4 v-bind:id="isSuccessed" v-html="nickName"></h4>
   <p v-html="hack"></p>
   <button v-bind:disabled="isDisabled">Bind Disabled</button>
+  <p v-bind:class="danger" class="underline">class binding</p>
+  <h2 v-bind:class="isPromoted && 'promoted'">promoted movie</h2>
+  <h2 v-bind:class="isSoldout ? 'sold-out' : 'new'">sold-out movie</h2>
+  <h2 v-bind:class="['new', 'promoted']">Newly promoted movie</h2>
+  <h2 v-bind:class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">
+    Array conditional movie
+  </h2>
+  <h2
+    v-bind:class="{
+      promoted: isPromoted,
+      new: !isSoldout,
+      'sold-out': isSoldout,
+    }"
+  >
+    Object conditional movie
+  </h2>
 </template>
 
 <script>
@@ -17,6 +33,9 @@ export default {
       isSuccessed: "success",
       hack: `<a href="#" onclick="alert('You have been HACKED!')"">Win a prize</a>`,
       isDisabled: true,
+      danger: "danger",
+      isPromoted: false,
+      isSoldout: true,
     };
   },
 };
@@ -33,5 +52,20 @@ export default {
 }
 #success {
   color: springgreen;
+}
+.danger {
+  color: red;
+}
+.underline {
+  text-decoration: underline;
+}
+.promoted {
+  font-style: italic;
+}
+.new {
+  color: green;
+}
+.sold-out {
+  color: red;
 }
 </style>
