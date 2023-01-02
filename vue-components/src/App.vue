026aca6e4496b959055dcd0ad90c1dfc1adf54e7
Sujon-Ahmed <sujonahmed5284@gmail.com>
2023-01-02 22:51:00 +0600

    Provide and Inject

--- vue-components/src/App.vue
+++ vue-components/src/App.vue
@@ -3,25 +3,32 @@
   <GreetComponent name="Clark" heroName="Superman" />
   <GreetComponent name="Daina" heroName="Wonder Woman" />
   <GreetComponent :name="name" :heroName="heroName" /> -->
-  <ArticleComponent id="my-article" title="Artcle title" :likes="50" :isPublished="true" />
+  <!-- <ArticleComponent id="my-article" title="Artcle title" :likes="50" :isPublished="true" /> -->
+  <h2>AppComponent username {{ name }}</h2>
+  <ComponentC />
 </template>
 
 <script>
 // import GreetComponent from "./components/GreetComponent.vue";
-import ArticleComponent from "./components/ArticleComponent.vue";
+// import ArticleComponent from "./components/ArticleComponent.vue";
+import ComponentC from "./components/ComponentC.vue";
 export default {
   name: "App",
   components: {
     // GreetComponent,
-    ArticleComponent
-
+    ComponentC,
   },
   data() {
-    return{
-      name: 'Sujon Ahmed',
-      heroName: 'Developer'
-    }
-  }
+    return {
+      name: "Sujon Ahmed",
+      heroName: "Developer",
+    };
+  },
+  provide() {
+    return {
+      username: this.name,
+    };
+  },
 };
 </script>
 
