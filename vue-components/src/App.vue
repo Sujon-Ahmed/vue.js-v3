c603478c2860363159a130e3f4d3aae219108733
Sujon-Ahmed <sujonahmed5284@gmail.com>
2023-01-06 20:42:38 +0600

    components events

--- vue-components/src/App.vue
+++ vue-components/src/App.vue
@@ -4,26 +4,37 @@
   <GreetComponent name="Daina" heroName="Wonder Woman" />
   <GreetComponent :name="name" :heroName="heroName" /> -->
   <!-- <ArticleComponent id="my-article" title="Artcle title" :likes="50" :isPublished="true" /> -->
-  <h2>AppComponent username {{ name }}</h2>
-  <ComponentC />
+  <!-- <h2>AppComponent username {{ name }}</h2> -->
+  <!-- <ComponentC /> -->
+  <button @click="showPopup = true">Show Popup</button>
+  <PopupComponent v-show="showPopup" @close="closePopup" />
 </template>
 
 <script>
 // import GreetComponent from "./components/GreetComponent.vue";
 // import ArticleComponent from "./components/ArticleComponent.vue";
-import ComponentC from "./components/ComponentC.vue";
+// import ComponentC from "./components/ComponentC.vue";
+import PopupComponent from "./components/PopupComponent.vue";
 export default {
   name: "App",
   components: {
     // GreetComponent,
-    ComponentC,
+    // ComponentC,
+    PopupComponent,
   },
   data() {
     return {
       name: "Sujon Ahmed",
       heroName: "Developer",
+      showPopup: false,
     };
   },
+  methods: {
+    closePopup(name) {
+      this.showPopup = false;
+      console.log('name:',name);
+    }
+  },  
   provide() {
     return {
       username: this.name,
---
+++ vue-components/src/components/PopupComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h3>This is simple popup</h3>
+        <button @click="$emit('close', 'Sujon')">Close Popup</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PopupComponent',
+        emits: ['close'],
+    }
+</script>
+
+<style scoped>
+
+</style>
